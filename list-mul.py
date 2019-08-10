49cdfcf89907ebcd996415cb5ca054980b0fc460
Mownicaraja <37542800+Mownicaraja@users.noreply.github.com>
2019-08-10 23:02:07 +0530

    Update list-mul.py

--- list-mul.py
+++ list-mul.py
@@ -2,7 +2,7 @@ def ml(li) :
     rslt = 1
     for x in li: 
          rslt = rslt * x  
-    return rslt  
+    return abs(rslt)  
 n=int(input())
 a=list(map(int,input().split()))[:n] 
 print(ml(a))
